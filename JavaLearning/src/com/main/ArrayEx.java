4694cb90e21ebfee91ed2e0a697a445fc62b0629
keerthanpm <41278625+keerthanpm@users.noreply.github.com>
2022-07-19 22:10:11 +0530

    first commit

---
+++ JavaLearning/src/com/main/ArrayEx.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		int mark[] = {67,68,69,70};
+		for(int i=0; i<mark.length;i++) System.out.println(mark[i]);
+		mark[0] = 45;
+		System.out.println(mark[0]);
+	}
+	
+
+}
